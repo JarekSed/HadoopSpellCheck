c6ef8ea6d410302d2af535e725e20aa87184687f
Jarek Sedlacek <jarek@cs.rutgers.edu>
2012-12-01 22:46:40 -0500

    Sorting previously only worked inside each reduce task, but didn't maintain a global order between reducers.
    Now, we distribute tasks between partitioners better. We sample
    the input to get a rough idea of it's distribution, and try to evenly
    distribute our key/value pairs among partitions while maintaining a
    global order.

--- WordCount.java
+++ WordCount.java
@@ -2,26 +2,181 @@ import java.io.IOException;
 import java.io.File;
 import java.net.URI;
 import java.util.*;
-
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.Configuration; 
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.util.GenericOptionsParser;
-
-
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.mapred.lib.IdentityMapper;
 import org.apache.hadoop.mapred.lib.IdentityReducer;
+import org.apache.hadoop.mapred.Partitioner;
+import org.apache.hadoop.fs.PathFilter;
 import org.apache.hadoop.mapred.lib.InputSampler;
 import org.apache.hadoop.mapred.lib.TotalOrderPartitioner;
-
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.io.RawComparator;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class WordCount {
+  private static String tmp_path_string = "/tmp/tmp_sort_path";
+  private static final Log LOG = LogFactory.getLog(WordCount.class);
+
+  public static class ReverseTotalOrderPartitioner <K extends WritableComparable, V> extends TotalOrderPartitioner<K, V> {
+  
+    public ReverseTotalOrderPartitioner() { }
+    @Override 
+    public int getPartition(K key, V value, int numPartitions) {
+        return numPartitions -1 - super.getPartition(key, value, numPartitions); 
+    } 
+  }
+
+  /* This is used to randomly sample some of our values so we can 
+   * get a rough estimate of the distribution. This is based on InputFormat.Sampler,
+   * but can't actually extend that interface because it samples values instead of keys
+   */
+   public static class ValueSampler<K, V> {
+   private double freq;
+   private int maxSplitsSampled;
+   private int numSamples;
+
+    public ValueSampler(double freq, int numSamples, int maxSplitsSampled) {
+      this.freq = freq;
+      this.maxSplitsSampled = maxSplitsSampled;
+      this.numSamples= numSamples;
+    } 
+
+    /*
+     * Randomize the split order, then take the specified number of keys from
+     * each split sampled, where each key is selected with the specified
+     * probability and possibly replaced by a subsequently selected key when
+     * the quota of keys from that split is satisfied.
+     */
+    public V[] getSample(InputFormat<K,V> inf, JobConf job) throws IOException {
+      InputSplit[] splits = inf.getSplits(job, job.getNumMapTasks());
+      ArrayList<V> samples = new ArrayList<V>(numSamples);
+      int splitsToSample = Math.min(maxSplitsSampled, splits.length);
+
+      Random r = new Random();
+      long seed = r.nextLong();
+      r.setSeed(seed);
+      LOG.debug("seed: " + seed);
+      // shuffle splits
+      for (int i = 0; i < splits.length; ++i) {
+        InputSplit tmp = splits[i];
+        int j = r.nextInt(splits.length);
+        splits[i] = splits[j];
+        splits[j] = tmp;
+      }
+      // our target rate is in terms of the maximum number of sample splits,
+      // but we accept the possibility of sampling additional splits to hit
+      // the target sample keyset
+      for (int i = 0; i < splitsToSample ||
+                     (i < splits.length && samples.size() < numSamples); ++i) {
+        RecordReader<K,V> reader = inf.getRecordReader(splits[i], job,
+            Reporter.NULL);
+        K key = reader.createKey();
+        V value = reader.createValue();
+        while (reader.next(key, value)) {
+          if (r.nextDouble() <= freq) {
+            if (samples.size() < numSamples) {
+              samples.add(value);
+            } else {
+              // When exceeding the maximum number of samples, replace a
+              // random element with this one, then adjust the frequency
+              // to reflect the possibility of existing elements being
+              // pushed out
+              int ind = r.nextInt(numSamples);
+              if (ind != numSamples) {
+                samples.set(ind, value);
+              }
+              freq *= (numSamples - 1) / (double) numSamples;
+            }
+            value = reader.createValue();
+          }
+        }
+        reader.close();
+      }
+      return (V[])samples.toArray();
+    }
+  }
+
+  /**
+   * Write a partition file for the given job, where the values
+   * in the partition file are the max value for each partition.
+   */
+  @SuppressWarnings("unchecked") // getInputFormat, getOutputKeyComparator
+  public static <K, V> void writePartitionFile(JobConf job,
+      ValueSampler<K,V> sampler) throws IOException {
+    final InputFormat<K,V> inf = (InputFormat<K,V>) job.getInputFormat();
+    int numPartitions = job.getNumReduceTasks();
+    V[] samples = sampler.getSample(inf, job);
+    LOG.info("Using " + samples.length + " samples");
+    RawComparator<V> comparator =
+      (RawComparator<V>) job.getOutputKeyComparator();
+    Arrays.sort(samples, comparator);
+    Path dst = new Path(ReverseTotalOrderPartitioner.getPartitionFile(job));
+    FileSystem fs = dst.getFileSystem(job);
+    if (fs.exists(dst)) {
+      fs.delete(dst, false);
+    }
+    SequenceFile.Writer writer = SequenceFile.createWriter(fs, job, dst,
+        job.getMapOutputKeyClass(), NullWritable.class);
+    NullWritable nullValue = NullWritable.get();
+    float stepSize = samples.length / (float) numPartitions;
+    int last = -1;
+    for(int i = 1; i < numPartitions; ++i) {
+      int k = Math.round(stepSize * i);
+      // TODO: check this condition?
+      
+      //Keep walking forward until we see something > than our last split point.
+      while (last >= 0 && comparator.compare(samples[last], samples[k]) >= 0) {
+        ++k;
+      }
+      LOG.info("Split with k" + k + " is " + samples[k].toString());
+      if (last > 0) {
+        LOG.info("last is " + last + " and has val " + samples[last].toString());
+      }
+      writer.append(samples[k], nullValue);
+      last = k;
+    }
+    writer.close();
+    
+  }
+
+
+
+
+    // This partitioner maintains order across different reduce tasks (by deciding which keys
+    // get send to which reducers). It currently makes the naive assumption that our input
+    // is uniformly distributed in the range 0 to INT_MAX. This range is divided by our # of
+    // reduce tasks, and each reducer becomes responsible for some subset of the range.
+    public static class OrderedPartitioner implements Partitioner<IntWritable, Text> {
+        @Override
+            public int getPartition(IntWritable key, Text value, int numPartitions) {
+                int num = key.get();
+                int splitPoint = Integer.MAX_VALUE / numPartitions;
+                int partition = num / splitPoint;
+                return partition < numPartitions ? partition : numPartitions -1;
+            }
+
+        public void configure(JobConf job) {}
+    }
+
+    // Only use part- files for input (or the directory they are in)
+    public static class InputFileFilter implements PathFilter {
+        @Override public boolean accept(Path path) {
+            return true;
+            //return path.toString() == tmp_path_string || path.getName().startsWith("part-");
+        }
+    }
 
     // This maps the text by counting how many times each word occurs. 
     // On each word, emits a <word, 1> pair
@@ -94,7 +249,7 @@ public class WordCount {
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-        Path tmp_path = new Path("/tmp/tmp_wordcount_sort"); 
+        Path tmp_path = new Path(tmp_path_string);
         if (otherArgs.length != 2) {
             System.err.println("Usage: wordcount <in> <out>");
             System.exit(2);
@@ -129,6 +284,7 @@ public class WordCount {
          * and IntWritable values, this run will sort them in decreasing order of value
          */
         Configuration newConf = new Configuration();
+        new GenericOptionsParser(newConf, args);
         JobConf jobConf = new JobConf(newConf, WordCount.class);
         tmp_path.getFileSystem(newConf).deleteOnExit(tmp_path);
         jobConf.setJobName("Word Count Sorted");
@@ -142,9 +298,22 @@ public class WordCount {
         jobConf.setOutputValueClass(Text.class);
         // Sort in reverse order.
         jobConf.setKeyFieldComparatorOptions("-r");
-
-        FileInputFormat.setInputPaths(jobConf, new Path(tmp_path, "part-00000"));
+        
+        FileInputFormat.addInputPath(jobConf, tmp_path);
         FileOutputFormat.setOutputPath(jobConf, new Path(otherArgs[1]));
+
+        // Sample our input so that we can partition our input evenly among all our reducers.
+        ValueSampler<Text, IntWritable> sampler = new ValueSampler<Text, IntWritable>(0.1,500,50);
+        Path partitionFile = new Path(tmp_path, "_sortPartitioning");
+        // Make sure a global order is maintained across reducers.
+        ReverseTotalOrderPartitioner.setPartitionFile(jobConf, partitionFile);
+        writePartitionFile(jobConf, sampler);
+        jobConf.setPartitionerClass(ReverseTotalOrderPartitioner.class);
+        // Make sure all reducers can see the artition file.
+        URI partitionUri = new URI(partitionFile.toString() +
+            "#" + "_sortPartitioning");
+        DistributedCache.addCacheFile(partitionUri, jobConf);
+        DistributedCache.createSymlink(jobConf);
 
         Date startTime = new Date();
         System.out.println("Job started: " + startTime);
@@ -153,6 +322,7 @@ public class WordCount {
         System.out.println("Job ended: " + end_time);
         System.out.println("The job took " + 
                 (end_time.getTime() - startTime.getTime()) /1000 + " seconds.");
+                
 
     }
 }
